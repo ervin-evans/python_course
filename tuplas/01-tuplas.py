7e9171b8641936dff2d4a96c4c9bb10e5abf627b
Ervin Triano <ervintdeveloper@gmail.com>
2025-04-20 19:24:45 -0500

    Se aprendio sobre tuplas

---
+++ tuplas/01-tuplas.py
@@ -0,0 +1,10 @@
+"""Una tupla es como una lista, pero inmutable (no puedes modificar sus elementos una vez creada)."""
+
+coordenadas = (10, 20)
+
+print(coordenadas[0])
+
+# Desempaquetando
+x, y = coordenadas
+
+print(f"X={x}, Y={y}")
---
+++ tuplas/Eje.ejercicio.py
@@ -0,0 +1,9 @@
+"""Ejercicio"""
+# Crea una tupla con tus datos personales: nombre, edad, ciudad
+datos_personales = ("Ervin", "30", "Tabasco")
+
+# Desempaquetando
+nombre, edad, ciudad = datos_personales
+
+# Imprime cada dato usando desempaquetado
+print(f"Nombre: {nombre} - Edad: {edad} - Ciudad: {ciudad}")
